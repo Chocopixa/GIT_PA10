f61ee31aa22624e3af6bccfa47bf7131c0ceecd8
Chocopixa <benben2212@outlook.com>
2023-10-23 14:27:46 +0800

    added mechanics
    completed the game :}

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,16 +1,21 @@
-﻿﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 //This script manages the behavior of individual obstacle
 public class Obstacle : MonoBehaviour
 {
     [SerializeField] private float Speed = 3;
-
     void Update()
     {
+        PosCheck();
+    }
+    private void PosCheck()
+    {
         if (transform.position.x <= -8)
+        {
+            Player.AddScore(1);
+            Debug.Log("added score");
             Destroy(gameObject);
+        }
         else
             transform.Translate(Vector3.right * Time.deltaTime * -Speed);
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    private Rigidbody thisRB;
+    private float Jumpforce = 9f;
+    public static int score;
+    public Text scoretxt;
 
     void Start()
     {
+        score = 0;
+        thisRB = GetComponent<Rigidbody>();
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
     }
 
     void Update()
     {
+        limitspeed();
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            thisRB.AddForce(Vector3.up * Jumpforce, ForceMode.Impulse);
             thisAnimation.Play();
+        }
+        UpdateText();
+    }
+    
+    private void limitspeed()
+    {
+        if(thisRB.velocity.y >= 8f)
+        {
+            thisRB.velocity = new Vector3(thisRB.velocity.x, 8f, thisRB.velocity.z);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("bottom"))
+        {
+            SceneManager.LoadScene("lose");
+        }
+    }
+    public static void AddScore(int scoreToAdd)
+    {
+        Player.score += scoreToAdd;
+    }
+    private void UpdateText()
+    {
+        scoretxt.text = "Score" + score.ToString();
     }
 }
